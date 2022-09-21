04ccf26fd1172993e2493a3a336e0d848a2fff8e
gosenx <pamossecuambe@gmail.com>
2022-09-21 19:08:09 +0200

    exercise 6 extra 8 solution

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -78,6 +78,7 @@ function App() {
         <ErrorBoundary
           FallbackComponent={ErrorFallback}
           onReset={() => setPokemonName('')}
+          resetKeys={[pokemonName]}
         >
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
